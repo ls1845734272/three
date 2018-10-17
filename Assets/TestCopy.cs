3acaef0834296725c7287c09018cdddf040756ce
ls1845737272 <1845734272@qq.com>
2018-10-17 12:10:29 +0800

    first

---
+++ Assets/TestCopy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestCopy : MonoBehaviour {
+	public Button btn;
+	public InputField input;
+	public Text text;
+	// Use this for initialization
+	void Start () {
+		btn.onClick.AddListener (CheckText);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void  CheckText()
+	{
+		TextEditor te = new TextEditor ();
+		if (input.text != "") {
+			Debug.Log ("input 是有值的");
+			te.text = input.text;
+			te.SelectAll ();
+			te.Copy ();
+		} else {
+			Debug.Log ("没有复制成功");
+		}
+	}
+
+}
